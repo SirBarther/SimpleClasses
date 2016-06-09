d906586ee67fd28351142f23c20610834c4da5af
Michael <michael.foster@outlook.com>
2016-06-08 20:08:01 -0400

    Finished class lesson.

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,7 +10,52 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Fusion";
+            myCar.Year = 2000;
+            myCar.Color = "Black";
 
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+
+            //decimal value = DetermineMarkedValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarkedValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
         }
     }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+
+            return carValue;
+        }
+    }
+
+
 }
